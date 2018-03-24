b1664405a289a11330d82402b8eef82b200bc367
Matthew Morgan <matt@dulcetsoftware.com>
2018-03-24 18:13:52 -0400

    Refactor

--- README.md
+++ README.md
@@ -2,28 +2,26 @@
 
 This URL is hardcoded in `/routes/index.js`
 
-## Docker deployment
-
-### Add the Sagoku remotes for understanding-proxy:
-
-If you haven't already:
-
+## Add your Sagoku remotes
 ```bash
 git remote add test git@git.test.cirrostratus.org:repos/understanding-proxy.git
 git remote add prod git@git.prod.cirrostratus.org:repos/understanding-proxy.git
 ```
 
+## Docker deployment
+
+- Build the node-injector-proxy container
+
 ```bash
-# Build the node-injector-proxy container
-#
-docker build -t node-injector-proxy .  # this will output the container hash at the end
-#
-# Tag and push the container
-#
+docker build -t node-injector-proxy .
+```
+
+- Tag and push the container
+
+```bash
 docker tag node-injector-proxy docker-registry.acorn.cirrostratus.org/playground/node-injector-proxy:<container-hash>
 docker push docker-registry.acorn.cirrostratus.org/playground/node-injector-proxy:<container-hash>
-#
-# Update the container hash in `docker-compose.yml`
-# Commit and push to GitHub
-# Commit and push to Sagoku
 ```
+- Update the container hash in `docker-compose.yml`
+- Commit and push to GitHub
+- Commit and push to Sagoku
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,43 @@
-const router = require('express').Router();
-const request = require('request');
+const router = require('express').Router()
+const request = require('request')
 const url = require('url')
 
-let currentHost = '';
+const INJECTED_JAVASCRIPT_URL = "https://s3.amazonaws.com/ithaka-labs/ugw-assets/ugw-test-latest.js"
+
+let proxyTargetHost = ''
 
 router.route('*')
-.get((req, res) => {
-  let uri = req.originalUrl.substring(1);
-  if (!uri.startsWith('http')){
-    uri=`${currentHost}/${uri}`
-  } else {
-    if (uri.startsWith('https')){
-      currentHost = `https://${uri.split('https://')[1].split('/')[0]}`
+  .get((req, res) => {
+    let uri = req.originalUrl.substring(1)
+    if (!uri.startsWith('http')) {
+      // relative URL most likely.  Prepend proxyTargetHost to URL.
+      uri = `${proxyTargetHost}/${uri}`
     } else {
-      currentHost = `http://${uri.split('http://')[1].split('/')[0]}`
+      extractProxyTargetHostFromRequest(uri)
     }
-  }
-  request({uri}, (err, response, data)=> {
-    if (err){
-      throw Error(err);
-    }
-    res.send(injectScriptTag(response.body));
-  });
-})
 
-function injectScriptTag(body){
+    // request.body exists courtesy of bodyParser middleware
+    request({uri}, (err, response, data) => {
+      if (err) {
+        throw Error(err)
+      }
+      res.send(injectScriptTag(response.body))
+    })
+  })
+
+function extractProxyTargetHostFromRequest(uri) {
+  if (uri.startsWith('https://')) {
+    proxyTargetHost = `https://${uri.split('https://')[1].split('/')[0]}`
+  } else {
+    proxyTargetHost = `http://${uri.split('http://')[1].split('/')[0]}`
+  }
+}
+
+function injectScriptTag(body) {
   return body.replace(
     '</html>',
-    '<script src="https://s3.amazonaws.com/ithaka-labs/ugw-assets/ugw-test-latest.js"></script></html>'
+    `<script src="${INJECTED_JAVASCRIPT_URL}"></script></html>`
   )
 }
 
-module.exports = router;
+module.exports = router
