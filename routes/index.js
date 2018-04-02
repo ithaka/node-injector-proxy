9a067ae97b3c372a9cfeaa44de82ea1ae86530f4
Matthew Morgan <mail.matt.morgan@gmail.com>
2018-04-02 13:48:35 -0400

    Refactor code

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,23 @@ const router = require('express').Router()
 const request = require('request')
 const url = require('url')
 
-const INJECTED_JAVASCRIPT_URL = "https://s3.amazonaws.com/ithaka-labs/ugw-assets/ugw-test-latest.js"
+const INJECTED_JAVASCRIPT_URL = 'https://s3.amazonaws.com/ithaka-labs/ugw-assets/ugw-test-latest.js'
 
 let proxyTargetHost = ''
 
 router.route('*')
   .get((req, res) => {
     let uri = req.originalUrl.substring(1)
-    if (!uri.startsWith('http')) {
-      // relative URL most likely.  Prepend proxyTargetHost to URL.
+    if (hasNoProtocol(uri)) {
+      // relative URL from previous host.  Prepend proxyTargetHost to URL.
       uri = `${proxyTargetHost}/${uri}`
     } else {
-      extractProxyTargetHostFromRequest(uri)
+      // new host or URL with protocol
+      proxyTargetHost = extractProxyTargetHostFromRequest(uri)
     }
 
     // request.body exists courtesy of bodyParser middleware
-    request({uri}, (err, response, data) => {
+    request({ uri }, (err, response, data) => {
       if (err) {
         throw Error(err)
       }
@@ -25,12 +26,15 @@ router.route('*')
     })
   })
 
+
+function hasNoProtocol(uri){
+  return !uri.startsWith('http')
+}
+
 function extractProxyTargetHostFromRequest(uri) {
-  if (uri.startsWith('https://')) {
-    proxyTargetHost = `https://${uri.split('https://')[1].split('/')[0]}`
-  } else {
-    proxyTargetHost = `http://${uri.split('http://')[1].split('/')[0]}`
-  }
+  return uri.startsWith('https://')
+    ? `https://${uri.split('https://')[1].split('/')[0]}`
+    : `http://${uri.split('http://')[1].split('/')[0]}`
 }
 
 function injectScriptTag(body) {
