3d1d867747c9cbd8088ad3ad87a8fe98accf2b09
Matthew Morgan <mail.matt.morgan@gmail.com>
2018-04-02 13:09:47 -0400

    Add error message template

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const index = require('./routes/index')
+const app = express()
 
-const index = require('./routes/index');
+app.use(bodyParser.text())
+app.use('*', index)
 
-const app = express();
-
-app.use(bodyParser.text());
-app.use('*', index);
+app.set('view engine', 'ejs')
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.send('error');
-});
+  res.status(err.status || 500)
+  res.render('error', {error: res.locals.err ? err.message : ''})
+})
 
-module.exports = app;
+
+module.exports = app
